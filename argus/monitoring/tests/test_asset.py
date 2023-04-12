43ba6561be2a9d8490673edf7726d44a6bd4a691
ben3329 <dnjsgur2634@gmail.com>
2023-04-12 11:51:13 +0900

    refactor: Change file name
    * api_params.py -> swagger_schema.py
    
    Resolves: #11

--- argus/monitoring/tests/test_asset.py
+++ argus/monitoring/tests/test_asset.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from ..models import *
-from ..api_params import *
+from ..swagger_schema import *
 import math
 
 
