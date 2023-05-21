da80ef6e3c434d35340c4d79f0ad49ca8074b339
ben3329 <dnjsgur2634@gmail.com>
2023-05-21 16:52:08 +0900

    정리

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+NPMDIR="`pwd`/argus/static/npm"
+npm install --prefix ${NPMDIR} bootstrap-icons@1.10.3
+npm install --prefix ${NPMDIR} bootstrap@5.2.3
+npm install --prefix ${NPMDIR} chart.js@4.3.0
+npm install --prefix ${NPMDIR} jquery@3.6.3
 cd argus
 DOCKER_BUILDKIT=1 docker build -t argus:latest .
 cd ../scrape
